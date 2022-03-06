0e67d753741b153e383aa187aec99f389a47373d
Elcio R <elcioroberto1995@gmail.com>
2022-03-06 18:30:16 -0300

    deleteando arquivo apos request

--- chapter-2/typescript-2/src/server.ts
+++ chapter-2/typescript-2/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { router } from './routes';
-import { Multer } from 'multer'
-
+import swaggerUi from 'swagger-ui-express';
+const swaggerFile = require("./swagger.json");
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(3333, () => {
     console.log("server is running")
