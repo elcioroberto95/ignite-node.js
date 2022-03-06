37904ea29325025aeb02f765e2364f6f0c8c352c
Elcio R <elcioroberto1995@gmail.com>
2022-03-06 13:33:01 -0300

    ajustando a rota de import adicionando a função controller

--- chapter-2/typescript-2/src/routes/categories.routes.ts
+++ chapter-2/typescript-2/src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { response, Router } from 'express';
 import multer from 'multer';
 import { createCategoryController } from '../modules/cars/useCases/createCategories';
-import { ImportCategoriesController } from '../modules/cars/useCases/importCategories/ImportCategoriesController';
+import { importCategoriesController } from '../modules/cars/useCases/importCategories';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 const categoriesRoutes = Router();
 const upload = multer({
@@ -22,7 +22,8 @@ categoriesRoutes.get('/', (req, res) => {
 
 
 categoriesRoutes.post('/import', upload.single("file"), (req, res) => {
-    
+    console.log("oi")
+    return importCategoriesController.handle(req, res);
 })
 export {
     categoriesRoutes
